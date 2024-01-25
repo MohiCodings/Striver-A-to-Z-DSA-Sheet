ad31c0713ace09b0cafc76914b2998b270248630
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-25 13:39:07 +0000

    Update 1) User input output.cpp

--- Learn the Basic/Things to know in c++/1) User input output.cpp
+++ Learn the Basic/Things to know in c++/1) User input output.cpp
@@ -1,1 +1,36 @@
+Problem statement
+Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
 
+
+
+1, if the character is an uppercase alphabet (A - Z).
+0, if the character is a lowercase alphabet (a - z).
+-1, if the character is not an alphabet.
+
+
+Example:
+Input: The character is 'a'.
+
+Output: 0
+
+Explanation: The input character is lowercase, so our answer is 0.
+
+
+Solution
+
+#include<iostream>
+using namespace std; 
+
+int main() {
+    char ch; 
+    cin >> ch; 
+    if(ch >= 'A' && ch <= 'Z') { 
+        cout << "1"; 
+    } 
+    else if(ch >= 'a' && ch <= 'z') {
+        cout << "0"; 
+    } 
+    else{
+        cout << "-1";
+    }
+}
