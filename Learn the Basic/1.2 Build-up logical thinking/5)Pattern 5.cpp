d81c5c99400928db9f661a4e140662e31f00fb2a
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-26 07:29:17 +0000

    Create 5)Pattern 5.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/5)Pattern 5.cpp
@@ -0,0 +1,25 @@
+Problem statement
+Sam is planting trees on the upper half region (separated by the left diagonal) of the square shared field.
+
+For every value of ‘N’, print the field if the trees are represented by ‘*’.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* *
+*
+
+  ## Solution
+
+  void seeding(int n) {
+	// Write your code here.
+
+	for(int i=0; i<n; i++){
+		for(int j=n; j>i; j--){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
