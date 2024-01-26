c429354ad3f0e41bbca9a890aa654c774c851d0c
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-26 06:48:04 +0000

    Update 1) Pattern 1.cpp

--- Learn the Basic/1.2 Build-up logical thinking/1) Pattern 1.cpp
+++ Learn the Basic/1.2 Build-up logical thinking/1) Pattern 1.cpp
@@ -14,4 +14,15 @@ Output:
 
   ##  Solution 
 
+  void nForest(int n) {
+	// Write your code here.
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      cout <<"* ";
+    }
+    cout <<endl;
+  }
+ 
+}
+
   
