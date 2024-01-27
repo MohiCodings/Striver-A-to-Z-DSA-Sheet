d68552efac632781a23b83fd73e8f48bcefcaba0
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-27 15:37:44 +0000

    Create 13)Pattern 13.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/13)Pattern 13.cpp
@@ -0,0 +1,26 @@
+Problem statement
+Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1
+2 3
+4 5 6
+
+
+  ## Solution
+
+  void nNumberTriangle(int n) {
+    // Write your code here.
+  int num = 1; // starting number
+    for(int i=1 ; i<=n; i++){  //  no of rows
+        for(int j=1; j<=i; j++){ // inner loop for printing i times
+            cout<<num<<" ";  // print number ncreasing by 1 each time
+            num= num + 1;
+        }
+        cout<<endl;
+    }
+}
