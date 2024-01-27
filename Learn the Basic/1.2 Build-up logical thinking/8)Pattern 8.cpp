e070ecddd3cf6d659c9974447ae82ef8d711d0f1
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-27 06:15:32 +0000

    Create 8)Pattern 8.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/8)Pattern 8.cpp
@@ -0,0 +1,32 @@
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the Reverse N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*****
+ ***
+  *
+
+
+  ## Solution 
+  void nStarTriangle(int n) {
+    // Write your code here.
+
+
+    for(int i=0; i<n; i++){  //outer loop for rows
+        for(int j=0; j<i; j++){  // printing spaces in row before star
+            cout<<" ";
+        }
+
+        for(int j=0; j<2*n-(2*i+1); j++){  // for printing stars n each row
+            cout<<"*";
+        }
+        for(int j=0; j<i; j++){  // For printing the spaces after the stars in each row
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
