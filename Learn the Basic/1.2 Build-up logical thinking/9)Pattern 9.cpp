987947adb70fb2003e29d59c4852442cef3c2827
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-27 06:33:47 +0000

    Create 9)Pattern 9.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/9)Pattern 9.cpp
@@ -0,0 +1,91 @@
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Diamond.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*****
+ ***
+  *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+  *
+ ***
+*****
+*****
+ ***
+  *    
+
+
+
+
+    ## Solution 
+    void nStarDiamond(int n) {
+    // Write your code here.
+    for (int i = 0; i < n; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<n-i-1; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*i+1;j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<n-i-1; j++)
+        {
+            cout <<" ";
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+
+
+//void inverted_pyramid(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < n; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*n -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+}
+}
