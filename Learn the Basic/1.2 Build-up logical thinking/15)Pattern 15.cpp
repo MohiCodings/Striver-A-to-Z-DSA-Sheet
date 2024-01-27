f0f226ea23b66791aa8d8c4aae1518ca4f9f30e7
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-27 16:18:36 +0000

    Create 15)Pattern 15.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/15)Pattern 15.cpp
@@ -0,0 +1,27 @@
+Problem statement
+Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Reverse Letter Triangle.
+
+You must print a matrix corresponding to the given Reverse Letter Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+A B C
+A B
+A
+
+## Solution
+
+void nLetterTriangle(int n) {
+    // Write your code here.
+
+    for( int i=0; i<n; i++){  // no of rows
+        for(char ch='A'; ch<='A'+(n-i-1); ch++){  // Inner loop will loop for i times and
+          // print alphabets from A to A + (N-i-1)
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
