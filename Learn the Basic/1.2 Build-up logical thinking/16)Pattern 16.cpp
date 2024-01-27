f739dc69818d67b04b51f5ab515b8f9c72609761
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-27 16:33:11 +0000

    Create 16)Pattern 16.cpp

---
+++ Learn the Basic/1.2 Build-up logical thinking/16)Pattern 16.cpp
@@ -0,0 +1,33 @@
+Problem statement
+Sam is curious about Alpha-Ramp, so he decided to create Alpha-Ramp of different sizes.
+
+An Alpha-Ramp is represented by a triangle, where alphabets are filled from the top in order.
+
+For every value of ‘N’, help sam to return the corresponding Alpha-Ramp.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+A
+B B
+C C C
+
+
+## Solution
+
+void alphaRamp(int n) {
+    // Write your code here.
+
+    for(int i=0;i<n;i++){  // for rows
+    // Defining character for each row.
+          char ch = 'A'+i;
+          for(int j=0;j<=i;j++){
+              
+              // same char is to be printed i times in that row.
+              cout<<ch<<" ";
+              
+          }
+          cout<<endl;
+}
+}
