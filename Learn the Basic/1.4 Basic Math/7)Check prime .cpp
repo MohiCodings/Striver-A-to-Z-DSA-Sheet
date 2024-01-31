387a2cadaefe16fbc67e60f5e685072b0df2dc82
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-31 06:42:05 +0000

    Create 7)Check prime .cpp

---
+++ Learn the Basic/1.4 Basic Math/7)Check prime .cpp
@@ -0,0 +1,35 @@
+Problem statement
+A prime number is a positive integer that is divisible by exactly 2 integers, 1 and the number itself.
+
+
+
+You are given a number 'n'.
+
+
+
+Find out whether 'n' is prime or not.
+
+
+
+Example :
+Input: 'n' = 5
+
+Output: YES
+
+Explanation: 5 is only divisible by 1 and 5. 2, 3 and 4 do not divide 5.
+
+
+
+bool isPrime(int n)
+{
+	// Write your code here.
+
+if(n == 2 || n == 3) return true; 
+	if(n == 1 || n%2 == 0 || n%3 == 0) return false; 
+
+	for(int i = 5; i*i<n; i+=6){
+		if(n%(i) == 0 || n%(i+2) == 0) return false; 
+	}
+
+	return true; 
+}
