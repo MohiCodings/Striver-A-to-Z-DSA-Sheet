20ce90c6cdb0a08c2fa8c78532ae0de43165dc37
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-01-31 07:47:38 +0000

    Create Print n times.cpp

---
+++ Learn the Basic/1.5 Basic Recursion/Print n times.cpp
@@ -0,0 +1,36 @@
+Problem statement
+You are given an integer ‘n’.
+
+
+
+Print “Coding Ninjas ” ‘n’ times, without using a loop.
+
+
+
+Example:
+Input: ‘n’  = 4
+
+Output:
+Coding Ninjas Coding Ninjas Coding Ninjas Coding Ninjas 
+
+Explanation: “Coding Ninjas” is printed 4 times. 
+
+
+
+
+
+  vector<string> printNTimes(int n) {
+	// Write your code here.
+
+	vector<string> ans;
+	for(int i = 0; i < n; i++) {
+		ans.push_back("Coding Ninjas");
+	}
+	return ans;
+}
+
+
+
+
+
+
