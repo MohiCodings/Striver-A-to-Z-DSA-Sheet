1e29f83225affba5bef5418c8237fff3edd8e16a
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-04 10:51:03 +0000

    Create reverse array.cpp

---
+++ Learn the Basic/1.5 Basic Recursion/reverse array.cpp
@@ -0,0 +1,38 @@
+Problem statement
+Given an array 'arr' of size 'n'.
+
+
+
+Return an array with all the elements placed in reverse order.
+
+
+
+Note:
+You don’t need to print anything. Just implement the given function.
+Example:
+Input: n = 6, arr = [5, 7, 8, 1, 6, 3]
+
+Output: [3, 6, 1, 8, 7, 5]
+
+Explanation: After reversing the array, it looks like this [3, 6, 1, 8, 7, 5].
+
+
+
+
+
+
+
+  void solve(int n, vector<int> &nums){
+    if(nums.empty()) return; 
+    int element = nums[0]; 
+    nums.erase(nums.begin()); 
+    solve(n, nums); 
+    nums.push_back(element); 
+}
+
+vector<int> reverseArray(int n, vector<int> &nums)
+{
+//    solve(n, nums);
+    reverse(nums.begin(), nums.end()); 
+   return nums; 
+}
