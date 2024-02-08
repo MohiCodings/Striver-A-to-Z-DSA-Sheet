beed57b3ed72551977d1477241d1e162826d6bf0
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-08 15:18:06 +0000

    Update 5)Armstrong Number.cpp

--- Learn the Basic/1.4 Basic Math/5)Armstrong Number.cpp
+++ Learn the Basic/1.4 Basic Math/5)Armstrong Number.cpp
@@ -1,15 +1,10 @@
-Problem statement
+**********************Problem statement****************************
 You are given an integer 'n'.
-
-
 
 Return 'true' if 'n' is an Armstrong number, and 'false' otherwise.
 
-
-
 Note:
 An Armstrong number is a number (with 'k' digits) such that the sum of its digits raised to 'kth' power is equal to the number itself. For example, 371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371.
-
 
 Example:
 Input: 'n' = 1634
@@ -19,7 +14,7 @@ Output: true
 Explanation:
 1634 is an Armstrong number, as 1^4 + 6^4 + 3^4 + 4^4 = 1634
 
-
+************************************************************************
 
 
   bool checkArmstrong(int n){
