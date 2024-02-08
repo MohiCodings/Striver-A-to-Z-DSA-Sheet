2c020dbd560e6e8c58560c7cd7e478d9ce1e5d72
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-08 15:18:47 +0000

    Update 6)Print all divisors.cpp

--- Learn the Basic/1.4 Basic Math/6)Print all divisors.cpp
+++ Learn the Basic/1.4 Basic Math/6)Print all divisors.cpp
@@ -1,15 +1,9 @@
-Problem statement
+**********************Problem statement**************************
 You are given an integer ‘n’.
-
-
 
 Function ‘sumOfDivisors(n)’ is defined as the sum of all divisors of ‘n’.
 
-
-
 Find the sum of ‘sumOfDivisors(i)’ for all ‘i’ from 1 to ‘n’.
-
-
 
 Example:
 Input: ‘n’  = 5
@@ -26,7 +20,7 @@ We need to find the sum of ‘sumOfDivisors(i)’ for all ‘i’ from 1 to 5.
 Therefore our answer is sumOfDivisors(1) + sumOfDivisors(2) + sumOfDivisors(3) + sumOfDivisors(4) + sumOfDivisors(5) = 1 + 3 + 4 + 7 + 6 = 21.
 
 
-
+******************************************************************************
 
   int sumOfAllDivisors(int n){
 	// Write your code here.
