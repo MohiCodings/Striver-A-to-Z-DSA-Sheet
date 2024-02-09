58a6872ac8fcd1209f2c859063d84ddf43b0877e
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-09 05:56:35 +0000

    Update 1) User input output.cpp

--- Learn the Basic/Things to know in c++/1) User input output.cpp
+++ Learn the Basic/Things to know in c++/1) User input output.cpp
@@ -1,7 +1,5 @@
-Problem statement
+********************Problem statement*******************************
 Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
-
-
 
 1, if the character is an uppercase alphabet (A - Z).
 0, if the character is a lowercase alphabet (a - z).
@@ -15,7 +13,8 @@ Output: 0
 
 Explanation: The input character is lowercase, so our answer is 0.
 
-
+*********************************************************************
+    
 Solution
 
 #include<iostream>
