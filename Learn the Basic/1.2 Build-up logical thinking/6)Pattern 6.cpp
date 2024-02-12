8d837da75722acfecfc84e8c86b1841a093c6999
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-12 06:14:52 +0000

    Update 6)Pattern 6.cpp

--- Learn the Basic/1.2 Build-up logical thinking/6)Pattern 6.cpp
+++ Learn the Basic/1.2 Build-up logical thinking/6)Pattern 6.cpp
@@ -1,4 +1,4 @@
-Problem statement
+***************************************Problem statement***********************************
 Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the Reverse N-Number Triangle.
 
 Example:
@@ -10,6 +10,9 @@ Output:
 1 2
 1
 
+
+**********************************************************************************************
+  
   ## Solution
 
   void nNumberTriangle(int n) {
