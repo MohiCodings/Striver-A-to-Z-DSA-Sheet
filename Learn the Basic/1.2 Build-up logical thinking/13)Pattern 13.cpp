95e7feb06c57eca4a3af4ac2208cff6e47b98ee1
Mohinee M <91136987+MohiCodings@users.noreply.github.com>
2024-02-12 06:04:53 +0000

    Update 13)Pattern 13.cpp

--- Learn the Basic/1.2 Build-up logical thinking/13)Pattern 13.cpp
+++ Learn the Basic/1.2 Build-up logical thinking/13)Pattern 13.cpp
@@ -1,4 +1,4 @@
-Problem statement
+******************************Problem statement************************************
 Aryan and his friends are very fond of patterns. For a given integer ‘N’, they want to make the Increasing Number Triangle.
 
 Example:
@@ -11,6 +11,8 @@ Output:
 4 5 6
 
 
+  *******************************************************************************
+  
   ## Solution
 
   void nNumberTriangle(int n) {
